63940dac2e92b5c39f078f4f31824a8df26696dc
chikwandagames <chikwandagames@gmail.com>
2021-03-26 16:57:27 +0000

    server that serves only one client

---
+++ server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+// To run
+// 1. cd server
+// 2. ./server
+
+func main() {
+	// TODO: write server program to handle concurrent client connections.
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// If theres an error we continue to get the next client connection
+			continue
+		}
+		handleConn(conn)
+	}
+
+}
+
+// handleConn - utility function
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		// Write a response to the connection handler every second
+		_, err := io.WriteString(c, "response from server\n")
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}

e838e8554743d547d1466989a4de15437d9f504b
chikwandagames <chikwandagames@gmail.com>
2021-03-26 15:16:25 +0000

    theory

--- main.go
+++ main.go
@@ -3,3 +3,22 @@ package main
 func main() {
 
 }
+
+// GOUROUTINES
+// We can think of goroutines as user space threads managed by
+// the go runtime
+
+// Goroutines are extremely lightweigth. they start with 2KB of stack,
+// Which grows and shrinks as required
+
+// Low CPU overhead - three instructions per function call
+// This allows for creation of hundreds of thousands of goroutines in
+// the same address space
+
+// CHANNELS are used to for communication of data between goroutines
+// so that sharing of memory can be avoided
+
+// Context switching between goroutines is much cheaper than thread switching
+// as goroutines have less state to store
+
+// Go runtime

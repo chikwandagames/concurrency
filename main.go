c46f6df13e904c406bfbaaa42da73dfea926c94f
chikwandagames <chikwandagames@gmail.com>
2021-03-26 15:40:13 +0000

    starting goroutines, three alternatives

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func foo(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func main() {
+	// Direct call
+	foo("direct call")
+
+	// TODO: write goroutine with different variants for function call.
+
+	// goroutine function call
+	go foo("function call")
+
+	// goroutine with anonymous function
+	go func() {
+		foo("anonimous function")
+	}()
+
+	// goroutine with function value call
+	fval := foo
+	go fval("function value")
+
+	// because main goroutine completes execution before other goroutines
+	// we need wait for other goroutines to complete, so we can see their execution
+	// results, so we sleep the main go routine
+	fmt.Println("waiting for goroutines...")
+	time.Sleep(10000 * time.Microsecond)
+
+	fmt.Println("done..")
 }
 
 // GOUROUTINES

8a5903d6dccf3c51c19ba42a02a76601801fa97c
chikwandagames <chikwandagames@gmail.com>
2021-03-29 19:31:15 +0100

    blocking, unbuffered synchronous channel

--- main.go
+++ main.go
@@ -2,38 +2,19 @@ package main
 
 import "fmt"
 
-// range for
-// Iterates over values received from a channel
-// It automatically breaks, when the channel is closed
-// range, does not return the second "ok" boolean value
 func main() {
-
 	ch := make(chan int)
+
 	go func() {
+		defer close(ch)
+
 		for i := 0; i < 6; i++ {
-			// TODO: send iterator over channel
+			fmt.Printf("Sending: %d\n", i)
 			ch <- i
 		}
-
-		// Close channel once goroutine has sent all its values
-		close(ch)
 	}()
 
-	// in the main goroutin, range over channel to received values
-	// once the channel is closed the range will exit
 	for v := range ch {
-		fmt.Println(v)
+		fmt.Printf("Received: %v\n", v)
 	}
-
 }
-
-/*
-func main() {
-	go func() {
-		for i := 0; i < 6; i++ {
-			// TODO: send iterator over channel
-		}
-	}()
-	// TODO: range over channel to recv values
-}
-*/

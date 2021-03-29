6d64e5d6fe37eecea3f50386968e7cadb6a34e14
chikwandagames <chikwandagames@gmail.com>
2021-03-29 19:07:15 +0100

    range for over channel

--- main.go
+++ main.go
@@ -2,26 +2,38 @@ package main
 
 import "fmt"
 
+// range for
+// Iterates over values received from a channel
+// It automatically breaks, when the channel is closed
+// range, does not return the second "ok" boolean value
 func main() {
 
 	ch := make(chan int)
-	go func(a, b int) {
-		c := a + b
-		// Send c into the channel
-		ch <- c
-	}(1, 2)
+	go func() {
+		for i := 0; i < 6; i++ {
+			// TODO: send iterator over channel
+			ch <- i
+		}
 
-	// In the main goroutine, we get the value from the channel
-	r := <-ch
-	fmt.Printf("Computed value: %v \n", r)
+		// Close channel once goroutine has sent all its values
+		close(ch)
+	}()
+
+	// in the main goroutin, range over channel to received values
+	// once the channel is closed the range will exit
+	for v := range ch {
+		fmt.Println(v)
+	}
+
 }
 
 /*
 func main() {
-	go func(a, b int) {
-		c := a + b
-	}(1, 2)
-	// TODO: get the value computed from goroutine
-	// fmt.Printf("computed value %v\n", c)
+	go func() {
+		for i := 0; i < 6; i++ {
+			// TODO: send iterator over channel
+		}
+	}()
+	// TODO: range over channel to recv values
 }
 */
